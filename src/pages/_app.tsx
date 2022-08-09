c3f26c18d6b1edbb53434286ec14566b7d9ae6ef
FrancoBarbaro <franbarbaro1@gmail.com>
2022-08-09 21:16:40 +0200

    imported css files so the downloaded fonts work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import '@fontsource/patua-one/400.css';
+import '@fontsource/space-mono/400.css';
+import '@fontsource/varela-round/400.css';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-// import { SWRConfig } from 'swr';
 import { theme } from '../theme';
+// import { SWRConfig } from 'swr';
 // import { fetcher } from '~/common/clients/fetcher';
 
 const App = ({ Component, pageProps }: AppProps) => {
