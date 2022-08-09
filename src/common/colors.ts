f1c7e197d32b0cdb97fb69c8f291c4a8afad84fb
FrancoBarbaro <franbarbaro1@gmail.com>
2022-08-09 18:30:51 +0200

    added colors, symbols, types to 'common' directory

---
+++ src/common/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+	white: '#fff',
+	galacticPurple: '#561be0',
+	lightGray: '#b4b4b4',
+	spaceGray: '#343d52',
+	darkGray: '#242a38',
+};
---
+++ src/common/symbols.ts
@@ -0,0 +1,6 @@
+export const symbols = {
+	jupiter: '&#9795;',
+	saturn: '&#9796;',
+	uranus: '&#9954;',
+	neptune: '&#9798;',
+};
