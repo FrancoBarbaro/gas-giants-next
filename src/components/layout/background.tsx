a612fb245318eb4f1069b99cd93c2ad3585ee2f1
FrancoBarbaro <franbarbaro1@gmail.com>
2022-08-16 19:38:16 -0500

    spacing change

--- src/components/layout/background.tsx
+++ src/components/layout/background.tsx
@@ -8,7 +8,6 @@ type BackgroundProps = {
 };
 
 const fadeIn = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } });
-
 const fadeOut = keyframes({ from: { opacity: 1 }, to: { opacity: 0 } });
 
 const duration = 300;
