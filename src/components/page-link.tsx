141ff5c8be721bb49bf6c9571ec0d2d1fba3cef3
FrancoBarbaro <franbarbaro1@gmail.com>
2022-08-20 18:21:42 -0500

    updated prop type name

--- src/components/page-link.tsx
+++ src/components/page-link.tsx
@@ -2,14 +2,14 @@ import { Link as ChakraLink } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import type { FC, ReactNode } from 'react';
 
-type LinkProps = {
+type PageLinkProps = {
 	href: string;
 	children: ReactNode;
 	variant?: string;
 };
 
 // TODO: make this component flexible to more props, especially 'target', maybe just take 'props' and spread it. Chakra probably has a type for its Link component
-export const PageLink: FC<LinkProps> = ({ href, children, variant }) => (
+export const PageLink: FC<PageLinkProps> = ({ href, children, variant }) => (
 	<NextLink href={href} passHref>
 		<ChakraLink
 			variant={variant}
