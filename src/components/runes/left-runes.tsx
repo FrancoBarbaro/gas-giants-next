abcfdf1ab9bd08b45b0414262654434dc9ddb043
Franco Barbaro <franbarbaro1@gmail.com>
2022-09-14 22:14:29 -0500

    made margin y auto on rune flexbox

--- src/components/runes/left-runes.tsx
+++ src/components/runes/left-runes.tsx
@@ -4,7 +4,7 @@ import { PageLink } from '~/components/page-link';
 
 // TODO: find out why the runes are changing height depending on the main div
 export const LeftRunes: FC = () => (
-	<Flex as="aside" flexDir="column" h="50vh" justifyContent="space-between">
+	<Flex as="aside" flexDir="column" h="50vh" justifyContent="space-between" my="auto">
 		<PageLink href="/jupiter" variant="rune">
 			&#9795;
 		</PageLink>
--- src/components/runes/right-runes.tsx
+++ src/components/runes/right-runes.tsx
@@ -4,7 +4,7 @@ import { PageLink } from '~/components/page-link';
 
 // TODO: if the device is too small, make these not show so more space is saved for the main content
 export const RightRunes: FC = () => (
-	<Flex as="aside" flexDir="column" h="50vh" justifyContent="space-between">
+	<Flex as="aside" flexDir="column" h="50vh" justifyContent="space-between" my="auto">
 		<PageLink href="/uranus" variant="rune">
 			&#9954;
 		</PageLink>
