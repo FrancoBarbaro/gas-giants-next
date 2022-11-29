ed12d1daf23e65867064f27056c7313a360b4836
Franco Barbaro <franbarbaro1@gmail.com>
2022-11-29 11:43:12 -0600

    improved skeleton layout

--- src/components/planet-page/planet-page-skeleton.tsx
+++ src/components/planet-page/planet-page-skeleton.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, VStack } from '@chakra-ui/react';
+import { AspectRatio, Skeleton, VStack } from '@chakra-ui/react';
 import type { FC } from 'react';
 import { RoundedTextBox } from '~/components/rounded-box/rounded-text-box';
 
@@ -8,23 +8,30 @@ type PlanetPageSkeletonProps = {
 
 export const PlanetPageSkeleton: FC<PlanetPageSkeletonProps> = ({ planet }) => (
 	<>
-		<VStack mr={10} w="50%">
-			<VStack as="section" alignItems="initial" w="100%">
-				<RoundedTextBox textType="h1">{`About ${planet}:`}</RoundedTextBox>
-				<Skeleton w="100%" h={40} borderRadius={15} />
-			</VStack>
-			<VStack as="section" alignItems="initial" w="100%">
-				<RoundedTextBox textType="h2">Symbol:</RoundedTextBox>
-				<Skeleton w="100%" h={40} borderRadius={15} />
-			</VStack>
-		</VStack>
-		<VStack ml={10} w="50%">
-			<VStack as="section" alignItems="initial" w="100%">
-				<RoundedTextBox textType="h2">Fun Facts:</RoundedTextBox>
+		<VStack mr={10} w="50%" h="100%" justifyContent="space-between">
+			<VStack as="section" alignItems="initial" w="100%" mb={6}>
+				<RoundedTextBox textType="h1" mb={2}>{`About ${planet}:`}</RoundedTextBox>
 				<Skeleton w="100%" h={56} borderRadius={15} />
 			</VStack>
 			<VStack as="section" alignItems="initial" w="100%">
+				<RoundedTextBox textType="h2" mb={2}>
+					Symbol:
+				</RoundedTextBox>
 				<Skeleton w="100%" h={40} borderRadius={15} />
+			</VStack>
+		</VStack>
+		<VStack ml={10} w="50%" h="100%" justifyContent="space-between">
+			<VStack as="section" alignItems="initial" w="100%">
+				<RoundedTextBox textType="h2" mt={1} mb={3}>
+					Fun Facts:
+				</RoundedTextBox>
+				<Skeleton w="100%" h={56} borderRadius={15} />
+			</VStack>
+			<VStack as="section" alignItems="initial" w="100%">
+				{/* it might be a good idea to make the site scrollable if the width is under a ceratin number */}
+				<AspectRatio ratio={480 / 219} w="100%">
+					<Skeleton w="100%" borderRadius={15} />
+				</AspectRatio>
 			</VStack>
 		</VStack>
 	</>
