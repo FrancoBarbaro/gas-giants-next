bf32c97489d414a92102724071be146ce16f2543
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-15 16:06:41 +0200

    cleaned up responsiveness todo list

--- src/components/planet-page/planet-page-skeleton.tsx
+++ src/components/planet-page/planet-page-skeleton.tsx
@@ -6,16 +6,13 @@ type PlanetPageSkeletonProps = {
 	planet: string;
 };
 
-// TODO: responsiveness improvements list:
-// 1. If width is too small, make buttons smaller (decrease width, font size) to allow for space between them ✅
-// 2. Make the text in the footer be able to shift in position so it is never at the edge of the footer and never
-//		has to wrap to a new line, also decrease font size if necessary. ✅
-// 4. For small devices such as iPhones, the site must be scrollable, but for bigger ones it must not be scrollable ✅
-// 5. Make headers resize nicer when screen gets smaller (also, consider changing the first header to be the same size
-//		as the others)
-// 6. Main issue: each planet has a different amount of text for each section, so they will be different in size,
-//		so how do i make a generalized skeleton layout? this may be a non-issue as the loading will be quite fast
-// 7. For very small devices the sections should all stack vertically, so the fun facts section would be below the symbol section
+/* TODO: responsiveness improvements list:
+1. Make headers resize nicer when screen gets smaller (also, consider changing the first header to be the same size
+		as the others)
+2. Main issue: each planet has a different amount of text for each section, so they will be different in size,
+		so how do i make a generalized skeleton layout? this may be a non-issue as the loading will be quite fast
+3. For very small devices the sections should all stack vertically, so the fun facts section would be below the symbol section
+*/
 
 export const PlanetPageSkeleton: FC<PlanetPageSkeletonProps> = ({ planet }) => (
 	<>
