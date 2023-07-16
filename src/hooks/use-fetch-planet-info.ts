cd10376028c1021587316bff783e0485ca047f61
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-16 09:07:18 +0200

    moved fetcher func, added token logic to fetcher

--- src/hooks/use-fetch-planet-info.ts
+++ src/hooks/use-fetch-planet-info.ts
@@ -1,8 +1,21 @@
+import { User } from 'firebase/auth';
 import useSWR from 'swr';
 import { GasGiant, PlanetInfo } from '~/common/types';
+import type { UserState } from '~/context/firebase-auth-context';
 
-export const useFetchPlanetInfo = (planet: GasGiant) => {
-	const { data } = useSWR<{ info: PlanetInfo }>(`/api/planets/${planet}`);
+const fetcher = async (url: RequestInfo, user: UserState) => {
+	// TODO: think more about this case where the user is null
+	if (!user) return;
 
+	const token = await user.getIdToken();
+	const res = await fetch(url, { headers: { Authorization: `Bearer ${token}` } });
+	if (!res.ok) {
+		throw new Error('An error occurred while fetching the data!');
+	}
+	return res.json();
+};
+
+export const useFetchPlanetInfo = (planet: GasGiant, user: UserState) => {
+	const { data } = useSWR<{ info: PlanetInfo }>([`/api/planets/${planet}`, user], fetcher, { errorRetryCount: 2 });
 	return { data: data?.info };
 };
