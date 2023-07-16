248c5c928554ef2627b1b850120b1361555d80ce
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-16 15:08:36 +0200

    added todo

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,12 +1,7 @@
 import { Skeleton, Stack, Text } from '@chakra-ui/react';
 import type { FC } from 'react';
 
-const SkeletonLayout: FC = () => (
-	<Stack>
-		<Skeleton />
-	</Stack>
-);
-
+// TODO: add an ai chatbot on this page, it would replace the table and maybe the picture
 export const HomePage: FC = () => (
 	<Text as="h3" color="pink">
 		Content
