86f171e61c382ede1ee57ef4293e953b3685f5b9
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-16 09:16:46 +0200

    removed todo

--- src/hooks/use-fetch-planet-info.ts
+++ src/hooks/use-fetch-planet-info.ts
@@ -4,11 +4,11 @@ import { GasGiant, PlanetInfo } from '~/common/types';
 import type { UserState } from '~/context/firebase-auth-context';
 
 const fetcher = async (url: RequestInfo, user: UserState) => {
-	// TODO: think more about this case where the user is null
 	if (!user) return;
 
 	const token = await user.getIdToken();
 	const res = await fetch(url, { headers: { Authorization: `Bearer ${token}` } });
+	
 	if (!res.ok) {
 		throw new Error('An error occurred while fetching the data!');
 	}
