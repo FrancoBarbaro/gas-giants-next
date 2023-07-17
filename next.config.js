855260c50a9e1492bc6f9d4635e94536e710d7a6
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-17 21:37:24 +0200

    moved home page redirect logic

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
+	redirects: async () => [
+		{
+			source: '/',
+			destination: '/home',
+			permanent: true,
+		},
+	],
 };
 
 module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
 import type { NextPage } from 'next';
 
-export const getStaticProps = () => ({
-	redirect: {
-		destination: '/home',
-		permanent: true,
-	},
-	props: {},
-});
-
 const Index: NextPage = () => null;
 
 export default Index;
