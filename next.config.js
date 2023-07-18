7be057619be68ba7a4b5cf6712305b5d171c0b16
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-18 10:28:50 +0200

    made all invalid paths redirect to home page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
-	redirects: async () => [
-		{
-			source: '/',
-			destination: '/home',
-			permanent: true,
-		},
-	],
 };
 
 module.exports = nextConfig;
