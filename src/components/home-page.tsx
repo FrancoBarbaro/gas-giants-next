2f382ed483885b93ee3a23a451c385dd23554692
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-22 19:57:39 -0500

    removed unused imports

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, Stack, Text } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 // TODO: add an ai chatbot on this page, it would replace the table and maybe the picture
