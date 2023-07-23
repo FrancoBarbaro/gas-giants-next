9b02298e803516ed66f4ae7e49e4565fe1a8b22b
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-22 19:06:38 -0500

    replaced old context with app check + auth context

---
+++ src/context/firebase-context.tsx
@@ -0,0 +1,58 @@
+import { AppCheck, getToken, initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
+import { onAuthStateChanged, signInAnonymously } from 'firebase/auth';
+import { createContext, FC, ReactNode, useEffect, useState } from 'react';
+import { auth, firebaseApp } from '~/clients/firebase/firebase-app';
+
+type ContextType = {
+	authToken: string | null;
+	appCheckToken: string | null;
+};
+
+type FirebaseContextProviderProps = {
+	children: ReactNode;
+};
+
+interface WindowWithDebugToken extends Window {
+	FIREBASE_APPCHECK_DEBUG_TOKEN: boolean | string | undefined;
+}
+declare var window: WindowWithDebugToken;
+
+let appCheck: AppCheck | null = null;
+if (typeof window !== 'undefined') {
+	if (process.env.NODE_ENV === 'development') {
+		window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+	}
+
+	appCheck = initializeAppCheck(firebaseApp, {
+		provider: new ReCaptchaV3Provider('6Lfx5TUnAAAAAKmmh38KCTmAyjVwky-pLONOj0ya'),
+		isTokenAutoRefreshEnabled: true,
+	});
+}
+
+export const FirebaseContext = createContext<ContextType>({ authToken: null, appCheckToken: null });
+
+export const FirebaseContextProvider: FC<FirebaseContextProviderProps> = ({ children }) => {
+	const [authToken, setAuthToken] = useState<string | null>(null);
+	const [appCheckToken, setAppCheckToken] = useState<string | null>(null);
+
+	onAuthStateChanged(auth, async (user) => {
+		const newAuthToken = (await user?.getIdToken()) || null;
+		setAuthToken(newAuthToken);
+	});
+
+	useEffect(() => {
+		(async () => {
+			if (appCheck === null) {
+				setAppCheckToken(null);
+				return;
+			}
+
+			const newAppCheckToken = (await getToken(appCheck)).token;
+			setAppCheckToken(newAppCheckToken);
+
+			await signInAnonymously(auth);
+		})();
+	}, []);
+
+	return <FirebaseContext.Provider value={{ authToken, appCheckToken }}>{children}</FirebaseContext.Provider>;
+};
