5f4c96e7c9845135d11816681cb915790b7bda95
Franco Barbaro <franbarbaro1@gmail.com>
2023-07-26 23:18:56 -0500

    reverted stream-response

--- src/pages/api/ai/stream-response.ts
+++ src/pages/api/ai/stream-response.ts
@@ -7,10 +7,6 @@ const AiResponseQuerySchema = z.object({
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	// if (typeof EdgeRuntime !== 'string') {
-	// 	// dead-code elimination is enabled for the code inside this block
-	// }
-
 	const validatedSchemaResult = AiResponseQuerySchema.safeParse(req.body);
 
 	if (!validatedSchemaResult.success) {
@@ -24,41 +20,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	if (!authToken) return res.status(401).json({ message: 'Request did not include an auth token!' });
 	if (!appCheckToken) return res.status(401).json({ message: 'Request did not include a firebase app check token!' });
 
-	// // set headers to allow for a streaming response
-	// res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
-	// res.setHeader('Cache-Control', 'no-cache, no-transform');
-	// res.setHeader('X-Accel-Buffering', 'no');
-	// res.setHeader('Connection', 'keep-alive');
-
-	let responseStream = new TransformStream();
+	// set headers to allow for a streaming response
+	res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+	res.setHeader('Cache-Control', 'no-cache, no-transform');
+	res.setHeader('X-Accel-Buffering', 'no');
 
 	// get a stream from calling the OpenAI API and pipe it to our response
 	const stream = await getAiResponseStream(prompt);
-	stream.pipeTo(responseStream.writable);
-	// await stream.pipeTo(
-	// 	new WritableStream({
-	// 		write(chunk) {
-	// 			res.write(chunk);
-	// 		},
-	// 	}),
-	// );
+	await stream.pipeTo(
+		new WritableStream({
+			write(chunk) {
+				res.write(chunk);
+			},
+		}),
+	);
 
 	// signal the end of the response to stop the stream
-	// res.end();
-
-	return new Response(responseStream.readable, {
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			'Content-Type': 'text/event-stream; charset=utf-8',
-			Connection: 'keep-alive',
-			'Cache-Control': 'no-cache, no-transform',
-			'X-Accel-Buffering': 'no',
-			'Content-Encoding': 'none',
-		},
-	});
+	res.end();
 }
-
-// configure this file to use Edge runtime instead of Node
-export const config = {
-	runtime: 'edge',
-};
