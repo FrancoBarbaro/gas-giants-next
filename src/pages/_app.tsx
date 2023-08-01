ed177f5ecff47c08d4b4bcbe92cbfed46943b9aa
Franco Barbaro <franbarbaro1@gmail.com>
2023-08-01 15:51:26 -0500

    moved background component into background folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import '@fontsource/quicksand/600.css';
 import '@fontsource/space-mono/400.css';
 import '@fontsource/varela-round/400.css';
 import type { AppProps } from 'next/app';
-import { Background } from '~/components/layout/background';
+import { Background } from '~/components/background/background';
 import { ContentWrapper } from '~/components/layout/content-wrapper';
 import { FirebaseContextProvider } from '~/context/firebase-context';
 import '../styles/globals.css';
