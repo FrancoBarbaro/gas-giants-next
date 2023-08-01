3ff668b7c561cdb981c57fa97ddeafafb9956403
Franco Barbaro <franbarbaro1@gmail.com>
2023-08-01 16:04:56 -0500

    added shooting stars component to background

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -1,68 +1,15 @@
-import { Box, keyframes, usePrefersReducedMotion } from '@chakra-ui/react';
-import { FC, useRef } from 'react';
-import { SwitchTransition, Transition } from 'react-transition-group';
-import { colors } from '~/theme/colors';
+import { Box } from '@chakra-ui/react';
+import { FC } from 'react';
+import { BackgroundImage } from '~/components/background/background-image';
+import { ShootingStars } from '~/components/background/shooting-stars';
 
 type BackgroundProps = {
 	currentPath: string;
 };
 
-const fadeIn = keyframes({ from: { opacity: 0 }, to: { opacity: 1 } });
-const fadeOut = keyframes({ from: { opacity: 1 }, to: { opacity: 0 } });
-
-const duration = 300;
-
-const baseStyle = {
-	w: '100%',
-	h: '100%',
-	bgSize: 'cover',
-	bgRepeat: 'no-repeat',
-	bgColor: colors.spaceBlue,
-	bgPos: 'center',
-	top: 0,
-	left: 0,
-	zIndex: 1,
-};
-
-const transitionStyles = {
-	entering: { opacity: 0, sx: { animation: `${fadeIn} ${duration}ms linear` } },
-	entered: { opacity: 1 },
-	exiting: { opacity: 1, sx: { animation: `${fadeOut} ${Math.floor(duration * 0.8)}ms ease-in` } },
-	exited: { opacity: 0 },
-	unmounted: {},
-};
-
-const zoom = keyframes`
-	0% {
-		transform: scale(1);
-	}
-	50% {
-		transform: scale(1.2);
-	}
-	100% {
-		transform: scale(1);
-	}
-`;
-
-export const Background: FC<BackgroundProps> = ({ currentPath }) => {
-	const bgUrl = `url('/images${currentPath}bg.jpeg')`;
-	const nodeRef = useRef(null);
-	const prefersReducedMotion = usePrefersReducedMotion();
-
-	return (
-		<SwitchTransition mode="in-out">
-			<Transition key={bgUrl} timeout={duration} nodeRef={nodeRef}>
-				{(state) => (
-					<Box
-						{...baseStyle}
-						pos="absolute"
-						bgImg={bgUrl}
-						animation={prefersReducedMotion ? undefined : `${zoom} 30s linear infinite`}
-						ref={nodeRef}
-						{...transitionStyles[state]}
-					/>
-				)}
-			</Transition>
-		</SwitchTransition>
-	);
-};
+export const Background: FC<BackgroundProps> = ({ currentPath }) => (
+	<Box pos="absolute" w="100%" h="100%" top={0} left={0}>
+		<BackgroundImage currentPath={currentPath} />
+		<ShootingStars />
+	</Box>
+);
---
+++ src/components/background/shooting-stars.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react';
+import { ShootingStar } from '~/components/background/shooting-star';
+
+export const ShootingStars: FC = () => (
+	<>
+		<ShootingStar />
+		<ShootingStar />
+		<ShootingStar />
+		<ShootingStar />
+	</>
+);
