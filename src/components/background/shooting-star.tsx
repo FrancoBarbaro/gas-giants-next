1e6151e7b8f9c76f58afdbc7e893ffa717daffe9
Franco Barbaro <franbarbaro1@gmail.com>
2023-08-03 19:12:37 -0500

    populated shooting star component

--- src/components/background/shooting-star.tsx
+++ src/components/background/shooting-star.tsx
@@ -1,3 +1,48 @@
-import type { FC } from 'react';
+import { Box, keyframes } from '@chakra-ui/react';
+import { FC, useEffect, useState } from 'react';
+import { colors } from '~/theme/colors';
 
-export const ShootingStar: FC = () => <span></span>;
+const fly = keyframes({
+	from: { transform: 'rotate(315deg) translateX(0)' },
+	to: { transform: 'rotate(315deg) translateX(-150vh)', opacity: 0 },
+});
+
+const baseStyle = {
+	top: -5,
+	right: -5,
+	w: 1,
+	h: 1,
+	bg: colors.white,
+	borderRadius: '50%',
+	boxShadow: `0 0 0 4px rgba(255, 255, 255, 0.1),
+		0 0 0 8px rgba(255, 255, 255, 0.1), 0 0 0 6px rgba(255, 255, 255, 1)`,
+};
+
+const beforeStyle = {
+	content: '""',
+	pos: 'absolute',
+	top: '50%',
+	width: 96,
+	height: 0.5,
+	background: 'linear-gradient(90deg, white, transparent)',
+	transform: 'translateY(-50%)',
+};
+
+// returns a randomly generated duration (in seconds) rounded to one decimal place
+const generateAnimationDuration = () => {
+	const [min, max] = [1, 5];
+	const randomNum = Math.random() * (max - min) + min;
+	return Math.round(randomNum * 10) / 10;
+};
+
+export const ShootingStar: FC = () => {
+	const [animation, setAnimation] = useState<string | undefined>(undefined);
+
+	useEffect(() => {
+		const duration = generateAnimationDuration();
+		setAnimation(`${fly} ${duration}s linear infinite`);
+	}, []);
+
+	// the span acts as the star's head, and the "before" pseudo-element acts as the star's tail
+	return <Box as="span" pos="absolute" animation={animation} {...baseStyle} _before={beforeStyle} />;
+};
