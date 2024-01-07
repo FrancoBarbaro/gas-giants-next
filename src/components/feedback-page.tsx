f55f9009dac07a37e8591dc2296c21ee35c6f564
Franco Barbaro <franbarbaro1@gmail.com>
2024-01-06 23:33:28 -0600

    added todo

--- src/components/feedback-page.tsx
+++ src/components/feedback-page.tsx
@@ -2,6 +2,7 @@ import { Text } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 // TODO: complete this page
+// TODO: remember to structure the feedback section of the database such that a user can only write to thier own feedback section
 export const FeedbackPage: FC = () => (
 	<Text as="h3" color="pink">
 		Content
