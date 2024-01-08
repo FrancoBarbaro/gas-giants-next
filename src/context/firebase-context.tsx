61ce2dda7c92a65c5f35dd519ed3cf800e7e5833
Franco Barbaro <franbarbaro1@gmail.com>
2024-01-07 19:05:26 -0600

    made context surface firebase uid

--- src/context/firebase-context.tsx
+++ src/context/firebase-context.tsx
@@ -6,6 +6,7 @@ import { auth, firebaseApp } from '~/clients/firebase/firebase-app';
 type ContextType = {
 	authToken: string | null;
 	appCheckToken: string | null;
+	userId: string | null;
 };
 
 type FirebaseContextProviderProps = {
@@ -29,15 +30,18 @@ if (typeof window !== 'undefined') {
 	});
 }
 
-export const FirebaseContext = createContext<ContextType>({ authToken: null, appCheckToken: null });
+export const FirebaseContext = createContext<ContextType>({ authToken: null, appCheckToken: null, userId: null });
 
 export const FirebaseContextProvider: FC<FirebaseContextProviderProps> = ({ children }) => {
+	const [userId, setUserId] = useState<string | null>(null);
 	const [authToken, setAuthToken] = useState<string | null>(null);
 	const [appCheckToken, setAppCheckToken] = useState<string | null>(null);
 
 	onAuthStateChanged(auth, async (user) => {
 		const newAuthToken = (await user?.getIdToken()) || null;
+		const newUserId = user?.uid || null;
 		setAuthToken(newAuthToken);
+		setUserId(newUserId);
 	});
 
 	useEffect(() => {
@@ -54,5 +58,5 @@ export const FirebaseContextProvider: FC<FirebaseContextProviderProps> = ({ chil
 		})();
 	}, []);
 
-	return <FirebaseContext.Provider value={{ authToken, appCheckToken }}>{children}</FirebaseContext.Provider>;
+	return <FirebaseContext.Provider value={{ authToken, appCheckToken, userId }}>{children}</FirebaseContext.Provider>;
 };
