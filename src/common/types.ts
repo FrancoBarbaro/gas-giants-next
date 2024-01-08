2ba5d4301f87325cd1915c68424b9174b4974653
Franco Barbaro <franbarbaro1@gmail.com>
2024-01-07 19:04:45 -0600

    made axios return types generic

--- src/common/types.ts
+++ src/common/types.ts
@@ -8,3 +8,28 @@ export type PlanetInfo = {
 	imageAltText: string;
 	imageDesc: string;
 };
+
+export type LikedWebsiteOptions = 'Yes' | 'No' | 'Kind Of';
+export type FavoritePageOptions = 'Home' | 'Jupiter' | 'Saturn' | 'Uranus' | 'Neptune';
+
+export type UserFeedback = {
+	likedWebsite: LikedWebsiteOptions;
+	favoritePages: FavoritePageOptions[];
+	additionalFeedback: string;
+};
+
+type BaseAxiosReturnType = {
+	error: boolean;
+};
+
+type SuccessAxiosReturnType<T> = BaseAxiosReturnType & {
+	error: false;
+	data: T;
+};
+
+type FailedAxiosReturnType = BaseAxiosReturnType & {
+	error: true;
+	message: string;
+};
+
+export type AxiosReturnType<T> = SuccessAxiosReturnType<T> | FailedAxiosReturnType;
--- src/server/endpoints/planets/get-planet-info-from-name.ts
+++ src/server/endpoints/planets/get-planet-info-from-name.ts
@@ -1,12 +1,11 @@
 import { firebaseApiFetcher } from '~/clients/firebase/firebase-axios-functions';
-import { PlanetInfo } from '~/common/types';
-import { PlanetInfoReturnType } from '~/server/endpoints/planets/types';
+import { AxiosReturnType, PlanetInfo } from '~/common/types';
 
 export const getPlanetInfoFromName = async (
 	planet: string,
 	authToken: string,
 	appCheckToken: string,
-): Promise<PlanetInfoReturnType> => {
+): Promise<AxiosReturnType<PlanetInfo>> => {
 	const firebaseResponse = await firebaseApiFetcher<PlanetInfo>(`/${planet}`, authToken, appCheckToken);
 	if (!firebaseResponse.success) return { error: true, message: 'An error occured retrieving planet info!' };
 	return { error: false, data: firebaseResponse.data };
