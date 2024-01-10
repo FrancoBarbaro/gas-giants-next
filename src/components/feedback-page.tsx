4c4eb4068848b8cae48f26a6b1e43ad4c362681b
Franco Barbaro <franbarbaro1@gmail.com>
2024-01-09 21:43:29 -0600

    utilized feedback context for conditional rendering

--- src/components/feedback-page.tsx
+++ src/components/feedback-page.tsx
@@ -15,19 +15,21 @@ import {
 	Radio,
 	RadioGroup,
 	Stack,
+	Text,
 	Textarea,
 	useDisclosure,
 	useToast,
 } from '@chakra-ui/react';
-import { ChangeEvent, FC } from 'react';
+import { useRouter } from 'next/router';
+import { ChangeEvent, FC, useContext } from 'react';
+import { FeedbackContext } from '~/context/feedback-context';
 import { useStoreUserFeedback } from '~/hooks/use-store-user-feedback';
 import { colors } from '~/theme/colors';
 
-/* TODO: may want to hide feedback form after submission, could use a SwitchTransition to
-animate the form out, having a piece of state such as formSubmitted that is set to true after
-submission and then the form is only rendered if formSubmitted is false */
 export const FeedbackPage: FC = () => {
 	const { isOpen: alertDialogIsOpen, onToggle: toggleAlertDialog, onClose: closeAlertDialog } = useDisclosure();
+	const { hasSubmittedFeedback, setHasSubmittedFeedback } = useContext(FeedbackContext);
+	const router = useRouter();
 	const toast = useToast();
 	const {
 		likedWebsite,
@@ -52,12 +54,13 @@ export const FeedbackPage: FC = () => {
 		toast.promise(feedbackStoredPromise, {
 			success: {
 				title: 'Feedback recieved!',
-				duration: 2000,
+				description: 'Thank you for your feedback!',
+				duration: 4000,
 			},
 			error: {
 				title: 'Something went wrong!',
 				description: 'An error occured while submitting your feedback. Please try again.',
-				duration: 2000,
+				duration: 4000,
 			},
 			loading: {
 				title: 'Submitting feedback...',
@@ -67,66 +70,80 @@ export const FeedbackPage: FC = () => {
 		setLikedWebsite('');
 		setFavoritePages([]);
 		setAdditionalFeedback('');
+		setHasSubmittedFeedback(true);
+		router.replace('/home');
 	};
 
-	// TODO: make the form dimensions dynamic
+	// TODO: give the form responsive styling
 	return (
-		<form id="feedback" onSubmit={submitHandler}>
-			<FormControl as="fieldset" color={colors.white} bg={colors.bluePurple} opacity={0.85} p={10} borderRadius="lg">
-				<Stack spacing={7}>
-					<Stack spacing={0.5}>
-						<FormLabel as="legend">Did you like this website?</FormLabel>
-						<RadioGroup value={likedWebsite} onChange={setLikedWebsite}>
-							<HStack spacing={6}>
-								<Radio value="Yes">Yes</Radio>
-								<Radio value="No">No</Radio>
-								<Radio value="Kind Of">Kind Of</Radio>
-							</HStack>
-						</RadioGroup>
-					</Stack>
-					<Stack spacing={0.5}>
-						<FormLabel as="legend">Which page was your favorite?</FormLabel>
-						<CheckboxGroup value={favoritePages} onChange={setFavoritePages}>
-							<HStack spacing={6}>
-								<Checkbox value="Home">Home</Checkbox>
-								<Checkbox value="Jupiter">Jupiter</Checkbox>
-								<Checkbox value="Saturn">Saturn</Checkbox>
-								<Checkbox value="Uranus">Uranus</Checkbox>
-								<Checkbox value="Neptune">Neptune</Checkbox>
-							</HStack>
-						</CheckboxGroup>
-					</Stack>
-					<Stack spacing={0.5}>
-						<FormLabel as="legend">Additional Feedback:</FormLabel>
-						<Textarea
-							h={48}
-							resize="none"
-							placeholder="Tell us more about what you liked about the website or suggest a way that the site may be improved."
-							value={additionalFeedback}
-							onChange={(event) => setAdditionalFeedback(event.target.value)}
-						/>
-					</Stack>
-					<Popover
-						returnFocusOnClose={false}
-						isOpen={alertDialogIsOpen}
-						onClose={closeAlertDialog}
-						placement="top"
-						closeOnBlur={false}
+		<>
+			{!hasSubmittedFeedback && (
+				<form id="feedback" onSubmit={submitHandler}>
+					<FormControl
+						as="fieldset"
+						color={colors.white}
+						bg={colors.bluePurple}
+						opacity={0.85}
+						p={10}
+						borderRadius="lg"
 					>
-						<PopoverTrigger>
-							<Button color={colors.bluePurple} bg={colors.white} form="feedback" type="submit">
-								Submit Feedback
-							</Button>
-						</PopoverTrigger>
-						<PopoverContent bg={colors.magentaPurple}>
-							<PopoverHeader>Empty Fields</PopoverHeader>
-							<PopoverArrow bg={colors.magentaPurple} />
-							<PopoverCloseButton />
-							<PopoverBody>You must fill out all fields before submitting the feedback form.</PopoverBody>
-						</PopoverContent>
-					</Popover>
-				</Stack>
-			</FormControl>
-		</form>
+						<Stack spacing={7}>
+							<Stack spacing={0.5}>
+								<FormLabel as="legend">Did you like this website?</FormLabel>
+								<RadioGroup value={likedWebsite} onChange={setLikedWebsite}>
+									<HStack spacing={6}>
+										<Radio value="Yes">Yes</Radio>
+										<Radio value="No">No</Radio>
+										<Radio value="Kind Of">Kind Of</Radio>
+									</HStack>
+								</RadioGroup>
+							</Stack>
+							<Stack spacing={0.5}>
+								<FormLabel as="legend">Which page was your favorite?</FormLabel>
+								<CheckboxGroup value={favoritePages} onChange={setFavoritePages}>
+									<HStack spacing={6}>
+										<Checkbox value="Home">Home</Checkbox>
+										<Checkbox value="Jupiter">Jupiter</Checkbox>
+										<Checkbox value="Saturn">Saturn</Checkbox>
+										<Checkbox value="Uranus">Uranus</Checkbox>
+										<Checkbox value="Neptune">Neptune</Checkbox>
+									</HStack>
+								</CheckboxGroup>
+							</Stack>
+							<Stack spacing={0.5}>
+								<FormLabel as="legend">Additional Feedback:</FormLabel>
+								<Textarea
+									h={48}
+									resize="none"
+									placeholder="Tell us more about what you liked about the website or suggest a way that the site may be improved."
+									value={additionalFeedback}
+									onChange={(event) => setAdditionalFeedback(event.target.value)}
+								/>
+							</Stack>
+							<Popover
+								returnFocusOnClose={false}
+								isOpen={alertDialogIsOpen}
+								onClose={closeAlertDialog}
+								placement="top"
+								closeOnBlur={false}
+							>
+								<PopoverTrigger>
+									<Button color={colors.bluePurple} bg={colors.white} form="feedback" type="submit">
+										Submit Feedback
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent bg={colors.magentaPurple}>
+									<PopoverHeader>Empty Fields</PopoverHeader>
+									<PopoverArrow bg={colors.magentaPurple} />
+									<PopoverCloseButton />
+									<PopoverBody>You must fill out all fields before submitting the feedback form.</PopoverBody>
+								</PopoverContent>
+							</Popover>
+						</Stack>
+					</FormControl>
+				</form>
+			)}
+			{hasSubmittedFeedback && <Text color={colors.neonPink}>Thank you for your feedback!</Text>}
+		</>
 	);
 };
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,20 @@
 import { Text } from '@chakra-ui/react';
-import type { FC } from 'react';
+import { FC, useContext } from 'react';
 import { FooterContainer } from '~/components/footer/footer-container';
 import { PageLink } from '~/components/page-link';
+import { FeedbackContext } from '~/context/feedback-context';
 
-export const Footer: FC = () => (
-	<FooterContainer>
-		<Text textAlign="center">&copy; Franco Barbaro 2022. All Rights Reserved.</Text>
-		<PageLink href="/feedback" variant="gradient-underline">
-			Feedback
-		</PageLink>
-	</FooterContainer>
-);
+export const Footer: FC = () => {
+	const { hasSubmittedFeedback } = useContext(FeedbackContext);
+
+	return (
+		<FooterContainer>
+			<Text textAlign="center">&copy; Franco Barbaro 2022. All Rights Reserved.</Text>
+			{!hasSubmittedFeedback && (
+				<PageLink href="/feedback" variant="gradient-underline">
+					Feedback
+				</PageLink>
+			)}
+		</FooterContainer>
+	);
+};
