3780f59fc6a9201fd974a38e6d71f7814c0246e7
Franco Barbaro <franbarbaro1@gmail.com>
2024-05-29 23:49:44 -0500

    testing 16px for phones

--- src/common/dynamic-values.ts
+++ src/common/dynamic-values.ts
@@ -1,3 +1,3 @@
 export const dynamicMarginBottom = { xxs: 0, sm: 1, md: 2 };
 export const dynamicBorderRadius = { xxs: 'lg', xs: 10, sm: 'xl', md: 14, lg: '2xl', xxl: 20 };
-export const dynamicFontSize = { xxs: 12, xs: 'xs', sm: 'sm', md: 'sm', lg: 'md', xxl: '2xl' };
+export const dynamicFontSize = { xxs: 16, xs: 'xs', sm: 'sm', md: 'sm', lg: 'md', xxl: '2xl' };
