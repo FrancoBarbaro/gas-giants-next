ac15dfe26a880db4540f1b7ed3f5e48f8adb6fd6
Franco Barbaro <franbarbaro1@gmail.com>
2024-05-29 22:07:00 -0500

    TEST: changed input font family to see if mobile bug comes from that

--- src/components/ai-chatbot.tsx
+++ src/components/ai-chatbot.tsx
@@ -55,6 +55,7 @@ export const AiChatbot: FC = () => {
 				minH={{ xxs: '10%', xs: '15%', md: '10%', xxl: '15%' }}
 			>
 				<Input
+					fontFamily="serif"
 					id="ai-chatbot-prompt"
 					h="100%"
 					minH="100%"
